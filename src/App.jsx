d04757e89b20de5580514086bd477ee21150da0d
Wagner Pereira Rosario Junior <pvg12207@mg.senac.br>
2026-05-11 11:57:01 -0300

    descrição da mudança

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Cliente from "./Cliente";
 import Admin from "./Admin";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Cliente />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Cliente />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
   );
 }
 
